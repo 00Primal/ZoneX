7fe8154ec46e951451cf90dd5e9e1ad1d3b1d752
Ar5urOfficial <krawiecbartlomiej8@gmail.com>
2026-01-11 07:35:32 +0100

    Update theme.js

--- theme.js
+++ theme.js
@@ -1,17 +1,15 @@
 const toggleBtn = document.getElementById('theme-toggle');
 
-// Funkcja ustawiająca motyw
-function setTheme(theme) {
+function updateTheme(theme) {
     document.documentElement.setAttribute('data-theme', theme);
     localStorage.setItem('theme', theme);
-    toggleBtn.innerText = theme === 'light' ? "Tryb: Jasny" : "Tryb: Ciemny";
+    toggleBtn.innerText = (theme === 'light') ? "TRYB: JASNY" : "TRYB: CIEMNY";
 }
 
-// Sprawdź zapisany motyw przy starcie
 const savedTheme = localStorage.getItem('theme') || 'dark';
-setTheme(savedTheme);
+updateTheme(savedTheme);
 
 toggleBtn.addEventListener('click', () => {
     const currentTheme = document.documentElement.getAttribute('data-theme');
-    setTheme(currentTheme === 'light' ? 'dark' : 'light');
+    updateTheme(currentTheme === 'light' ? 'dark' : 'light');
 });
